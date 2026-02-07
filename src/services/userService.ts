deae6a34e8c68824da12e6d58bf9b23690bc9822
何红妤#A05018 <hongyu.he@casstime.com>
2026-02-07 20:48:18 +0800

    对用户密码加密

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,9 +1,19 @@
 import User from "../model/User";
 import { IRegisterData } from "../interface";
+import bcrypt from 'bcrypt';
+
+// 加密强度
+const SALT_ROUNDS = 10;
 
 export const registerUser = async (data: IRegisterData) => {
   try {
-    const user = new User(data);
+    // 加密密码
+    const hashPassword = await bcrypt.hash(data.password, SALT_ROUNDS);
+    // 创建用户，使用加密后的密码
+    const user = new User({
+      ...data,
+      password: hashPassword
+    });
     await user.save();
 
     return {
